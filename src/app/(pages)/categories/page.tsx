96747364bd8e56465dec933dbead9f3881f43f6e
MohamedKhaledElnahrawy <mohamedkhaledme1914@gmail.com>
2026-02-21 01:04:22 +0200

    edit style: in categories page

--- src/app/(pages)/categories/page.tsx
+++ src/app/(pages)/categories/page.tsx
@@ -18,7 +18,6 @@ export default async function Categories() {
   return (
     <div>
       <div className="max-w-7xl mx-auto p-6">
-        <h2 className="text-2xl font-bold mb-6">Categories Page</h2>
 
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
           {data?.data?.length > 0 ? (
